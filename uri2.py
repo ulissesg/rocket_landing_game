a0db4cf7458c02772347bd5a64288bfe91aff9a0
ulissesg <42587034+ulissesg@users.noreply.github.com>
2018-10-23 19:46:17 -0300

    Delete uri2.py

--- uri2.py
+++
@@ -1,7 +0,0 @@
-nome= input("digite o nome do funcionario:\n")
-sal = float (input("insira o salario fixo:\n"))
-vendas= float(input("insira o valor total de vendas:\n"))
-
-total= sal + (vendas*0.15)
-print("\nnome: ", nome)
-print("salario total: ", total)
