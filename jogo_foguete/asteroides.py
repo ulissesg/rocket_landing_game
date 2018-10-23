be2ce335018fbb3dc5dfcfba1264f5318440c992
ulisses <ulissesg2013@live.com>
2018-10-23 16:11:57 -0300

    17/10/18

---
+++ jogo_foguete/asteroides.py
@@ -0,0 +1,18 @@
+from pc_simples import *
+
+''' Asteroide eh um Pc_simples
+interp. representa um asteroide com suas posicoes e velocidades.
+'''
+#EXEMPLOS:
+ASTEROIDE_INICIAL = Pc_simples(300, 0, 1, 1)
+ASTEROIDE_MEIO = Pc_simples(300, 200, 1, 1)
+ASTEROIDE_FINAL = Pc_simples(300, 400, 1, 1)
+
+#TEMPLATE
+'''
+def fn_para_asteroide(ast):
+    ... ast.x
+        ast.y
+        ast.dx
+        ast.dy
+'''
