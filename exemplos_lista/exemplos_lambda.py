d1a37d5bb4219a809ee5de168d19bc691de1f9f0
ulisses <ulissesg2013@live.com>
2018-10-23 20:28:37 -0300

    deltei todas os outros arquivos, so ficou o jogo

--- exemplos_lista/exemplos_lambda.py
+++
@@ -1,7 +0,0 @@
-def dobro(x):
-    return x*2
-
-quadruplo = lambda x: x*4
-
-print(lambda x,y: x+y)
-print(quadruplo(4))
--- exercicios5/bolinha_e_quadrado.py
+++
@@ -1,9 +0,0 @@
-'''
-Coloque o quadrado giratorio e a bolinha dos
-exercícios anteriores no mesmo programa.
-Isto é, o programa deve consistir em uma animação
-com um quadrado giratório crescendo constantemente
-e uma bolinha "rolando" pela tela e quicando nas
-bordas também em velocidade constante.
-
-'''
